461ed081bf9640475763bcb2277592fdea0441ff
Galib-23 <aagalib2323@gmail.com>
2024-04-07 00:07:49 +0600

    Redux persist added

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { signInFailure, signInStart, signInSuccess } from "../redux/user/userSlice";
 
 const SignIn = () => {
 
   const [ formData, setFormData ] = useState({});
-  const [ error, setError ] = useState(null);
-  const [ loading, setLoading ] = useState(false);
+  const { loading, error } = useSelector((state) => state.user);
+  
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   
   const handleChange = (e) => {
     setFormData({
@@ -19,7 +22,7 @@ const SignIn = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      setLoading(true);
+      dispatch(signInStart())
       const res = await fetch('/api/auth/signin', {
         method: 'POST',
         headers: {
@@ -29,18 +32,15 @@ const SignIn = () => {
       });
       const data = await res.json();
       if(data?.success === false){
-        setError(data?.message);
-        setLoading(false);
+        dispatch(signInFailure(data?.message));
         return;
       }
-      setLoading(false);
-      setError(null);
+      dispatch(signInSuccess(data));
       navigate('/');
-      console.log(data);
+      //console.log(data);
 
     } catch (error) {
-      setLoading(false);
-      setError(error.message);
+      dispatch(signInFailure(error?.message));
     }
   }
 
