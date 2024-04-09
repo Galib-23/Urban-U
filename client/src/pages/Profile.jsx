fdae5553981a039982e8f7f66c85f9b02f856585
Galib-23 <aagalib2323@gmail.com>
2024-04-09 16:23:55 +0600

    Delete account created

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -10,6 +10,9 @@ import {
   updateUserStart,
   updateUserFailure,
   updateUserSuccess,
+  deleteUserFailure,
+  deleteUserStart,
+  deleteUserSuccess,
 } from "../redux/user/userSlice";
 import { app } from "../firebase";
 
@@ -85,6 +88,23 @@ const Profile = () => {
     }
   };
 
+  const handleDeleteUser = async () => {
+    try {
+      dispatch(deleteUserStart());
+      const res = await fetch(`/api/user/delete/${currentUser._id}`,{
+        method: 'DELETE',
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+    } catch (error) {
+      dispatch(deleteUserFailure(error.message));
+    }
+  }
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -153,7 +173,7 @@ const Profile = () => {
         </button>
       </form>
       <div className="flex justify-between items-center mt-5">
-        <span className="text-red-700 cursor-pointer hover:underline font-semibold hover:text-purple-500">
+        <span onClick={handleDeleteUser} className="text-red-700 cursor-pointer hover:underline font-semibold hover:text-purple-500">
           Delete Account
         </span>
         <span className="text-red-700 cursor-pointer font-semibold hover:underline hover:text-purple-500">
