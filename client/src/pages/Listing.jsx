82059027390165dcaac17a1b9096e119a5d9686a
Galib-23 <aagalib2323@gmail.com>
2024-04-13 21:00:41 +0600

    Complete header search functionality

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -87,6 +87,11 @@ const Listing = () => {
               }}
             />
           </div>
+          {copied && (
+            <p className='fixed top-[23%] right-[5%] z-10 rounded-md bg-slate-100 p-2'>
+              Link copied!
+            </p>
+          )}
           <div className="flex flex-col max-w-4xl mx-auto p-3 my-7 gap-4">
             <p className="text-2xl font-semibold">
               {listing.name} - ${" "}
