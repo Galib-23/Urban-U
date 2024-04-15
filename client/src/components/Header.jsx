bdb619b07d426dbdd372bafe8c779c312d2251b1
Galib-23 <aagalib2323@gmail.com>
2024-04-15 11:54:40 +0600

    Complete about page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -29,9 +29,9 @@ const Header = () => {
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to="/">
-          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-            <span className="text-slate-500">Rial</span>
-            <span className="text-slate-700">Estate</span>
+          <h1 className="font-bold text-sm sm:text-2xl flex flex-wrap">
+            <span className="text-slate-500">Urban</span>
+            <span className="text-slate-700">-U</span>
           </h1>
         </Link>
         <form onSubmit={handleSubmit} className="bg-slate-100 p-3 rounded-lg flex items-center">
--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,8 +1,11 @@
 
 const About = () => {
   return (
-    <div>
-      
+    <div className="py-20 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">About Urban-U</h1>
+      <p className="mb-4 text-slate-700">Welcome to Urban-U, your premier destination for all things real estate. At Urban-U, we are dedicated to simplifying the process of buying, selling, and renting properties, providing you with unparalleled convenience and efficiency every step of the way.</p>
+      <p className="mb-4 text-slate-700">With our user-friendly platform, finding your dream home or investment property has never been easier. Our advanced search and sorting tools allow you to filter listings based on your specific criteria, whether you are looking for a cozy apartment in the heart of the city, a spacious family home in the suburbs, or a luxurious waterfront villa with panoramic views.</p>
+      <p className="mb-4 text-slate-700">But Urban-U offers more than just a vast selection of properties. We empower you to take control of your real estate journey with our innovative features for managing your own listings. Whether you are a homeowner looking to sell or a landlord seeking tenants, our platform provides the tools you need to showcase your property to potential buyers and renters, maximizing your visibility and efficiency in the market.</p>
     </div>
   )
 }
